7e3c3a4859193cec8dd2fe499e066a9ce137ff0b
Pete <pe.czer3@gmail.com>
2022-02-22 21:33:48 -0500

    Add files via upload

---
+++ mixed-messages.js
@@ -0,0 +1,15 @@
+// mixed messager program; random message printout with each run
+
+// message array
+const messages = [
+    'Coding is another way of saying programming', 
+    'There is a unique use for each and every programming language', 
+    'Front-end programming is the user side, back-end programming is server side', 
+    'Programming has become easier than ever to learn', 
+    'Programming opens up your creativity in limitless ways', 
+    'Anybody can learn how to code'
+];
+
+
+
+
